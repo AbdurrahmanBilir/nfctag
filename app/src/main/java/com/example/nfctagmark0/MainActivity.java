d55d5bee9e05edd3ac04ce5ee0d3260a811afc5c
AbdurrahmanBilir <abdurrahmanb25@gmail.com>
2021-06-30 18:38:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nfctagmark0/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.nfctagmark0;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
