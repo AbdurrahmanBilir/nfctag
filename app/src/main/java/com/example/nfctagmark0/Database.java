cc46d7ae0456da31ba0ee9a4afeb55130ff94824
AbdurrahmanBilir <abdurrahmanb25@gmail.com>
2021-06-30 23:29:21 +0300

    Database class has added.

---
+++ app/src/main/java/com/example/nfctagmark0/Database.java
@@ -0,0 +1,23 @@
+package com.example.nfctagmark0;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
